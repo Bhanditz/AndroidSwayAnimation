758eff7777116d8dc849d85f551e7c4b38f07b2c
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2016-03-16 21:40:54 +0900

    fix sample

--- sample/src/main/java/com/kidach1/androidswayanimation/MainActivity.java
+++ sample/src/main/java/com/kidach1/androidswayanimation/MainActivity.java
@@ -3,10 +3,7 @@ package com.kidach1.androidswayanimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
-
 import com.kidach1.swayanimation.SwayAnimation;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 //                R.drawable.ic_tag_faces_amber_300_48dp,
 //                R.drawable.ic_thumb_up_blue_a200_48dp
 //        ));
-        SwayAnimation.setWithActionBar(true);
+//        SwayAnimation.setWithActionBar(true);
         SwayAnimation.ready(layout, layout, this);
     }
 }
